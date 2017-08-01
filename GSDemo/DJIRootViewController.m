a092dae6f45f8b7758c104f5183c92449f9592cf
Oliver Ou <oliver.ou@dji.com>
2017-08-01 21:37:00 +0800

    update demo with application activation.

--- GSDemo/DJIRootViewController.m
+++ GSDemo/DJIRootViewController.m
@@ -143,6 +143,14 @@
     }else{
         ShowMessage(@"Product disconnected", nil, nil, @"OK");
     }
+    
+    //If this demo is used in China, it's required to login to your DJI account to activate the application. Also you need to use DJI Go app to bind the aircraft to your DJI account. For more details, please check this demo's tutorial.
+    [[DJISDKManager userAccountManager] logIntoDJIUserAccountWithAuthorizationRequired:NO withCompletion:^(DJIUserAccountState state, NSError * _Nullable error) {
+        if (error) {
+            NSLog(@"Login failed: %@", error.description);
+        }
+    }];
+    
 }
 
 #pragma mark action Methods
